552163997131bfc204bb629a4ece209cb6c25efa
Allison Bickford <abickford@michiganlabs.com>
2021-08-02 09:12:31 -0700

    fixed index

--- _layouts/src/components/Topic/index.tsx
+++ _layouts/src/components/Topic/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react"
-import Topic from "./index"
-import "../../styles/main.scss"
-import "../../styles/overrides.scss"
-import Helmet from "react-helmet"
+import React, { useEffect } from "react";
+import Topic from "./index";
+import "../../styles/main.scss";
+import "../../styles/overrides.scss";
+import Helmet from "react-helmet";
 import { withPrefix } from "gatsby";
 
 
@@ -10,8 +10,6 @@ export default function MyTopic(props) {
   return (
     <div className="orta">
          <Helmet>
-          <meta property="twitter:domain" content="orta.io"/>
-          <meta name="twitter:card" content="summary_large_image"/>
           <meta property="og:image" content={"https://allisonbickford.github.io" + withPrefix(props.file.fields.slug + "/og-image.jpg")} />
         </Helmet>
       <Topic {...props} />
